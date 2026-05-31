69dac7b4fa10d817fda92594eb0627d62961299a
shlok2006-cd <shlokneupane05@gmail.com>
2026-05-31 10:12:29 +0545

    Established database connection with MongoDB

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require("dotenv").config({ path: "./env" });
+
+import dotenv from "dotenv";
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// import express from "express";
+
+// const app = express()(async () => {
+//   try {
+//     await mongoose.connnect(`${process.env.MONGODB_URI}`);
+//     app.on("error", () => {
+//       console.log("ERROR:", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR:", error);
+//     throw error;
+//   }
+// })();
